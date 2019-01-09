ce755a3bd368aa9f4c4320e3ab168e0a75ef12c6
PMuiruri <peninah.m.muiruri@gmail.com>
2019-01-09 11:59:28 +0200

    Added speetest react app

---
+++ build/precache-manifest.40fe94d3bf0cb244401a407edf478461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0b1329a55eab34a5ea73",
+    "url": "./static/js/main.0b1329a5.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "./static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "0b1329a55eab34a5ea73",
+    "url": "./static/css/main.44ee310a.chunk.css"
+  },
+  {
+    "revision": "bd7a8c54fadeb279473bfb9d09f91107",
+    "url": "./index.html"
+  }
+];
